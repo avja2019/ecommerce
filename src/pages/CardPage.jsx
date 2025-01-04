284f94e5774b64bb2d880748a5f824f1e285985f
Jhon Antony <jhonatoni9.81@gmail.com>
2025-01-03 20:16:13 -0500

    avance 3 price  and total

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getCardProdsThunk } from '../store/slices/cart.slice';
 import ItemCart from '../components/cartPage/ItemCart';
+import './styles/cartpage.css';
 
 const CardPage = () => {
 
@@ -16,18 +17,36 @@ const CardPage = () => {
   
   
   //console.log(cart);
+  const products = cart.reduce(
+    (cv, prod,) => cv+=prod.quantity,0
+  );
+
+  const total = cart.reduce(
+    (cv, prod,) => cv+= prod.quantity*prod.product.price,0
+  );
+
 
   return (
-    <div>
-      {
-        cart.map(prod => (
-          <ItemCart
-            key = {prod.id}
-            prod={prod}
-          />
-        ))
-      }
+    <div className='cartpage'>
+      <div className='cartpage_container'>
+        {
+          cart.map(prod => (
+            <ItemCart
+              key = {prod.id}
+              prod={prod}
+            />
+          ))
+        }
+      </div>
+      <div>
+        <ul>
+          <li><span>Products: </span><span>{products}</span></li>
+          <li><span>Total: </span><span>$ {total}</span></li>
+        </ul>
+        <button>Buy all Products</button>
+      </div>
     </div>
+    
   )
 }
 
